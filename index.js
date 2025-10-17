bbd83ddeb19ee36b233f14e0d60889ad60c2704b
AliMousaDev <alimousa20070@gmail.com>
2025-10-17 11:02:44 +0300

    Say my name !

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ require("dotenv").config();
 const translateHandler = require("./api/translate");
 const serveHandler = require("./api/serve");
 const downloadHandler = require("./api/download");
-<<<<<<< HEAD
 const anilistToHiAnimeHandler = require("./api/anilist-to-hianimez");
-=======
->>>>>>> origin/main
 
 const app = express();
 
@@ -47,12 +44,8 @@ app.get("/", (req, res) => {
     routes: [
       "/api/translate",
       "/api/serve",
-<<<<<<< HEAD
       "/api/download",
       "/api/anilist-to-hianimez"
-=======
-      "/api/download"
->>>>>>> origin/main
     ] 
   });
 });
@@ -60,10 +53,7 @@ app.get("/", (req, res) => {
 app.use("/api/translate", translateHandler);
 app.use("/api/serve", serveHandler);
 app.use("/api/download", downloadHandler);
-<<<<<<< HEAD
 app.use("/api/anilist-to-hianimez", anilistToHiAnimeHandler);
-=======
->>>>>>> origin/main
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
