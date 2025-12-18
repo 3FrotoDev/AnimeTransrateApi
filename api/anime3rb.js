b12acf6d5816754f5ea2c44b63780fda00db2546
AliMousaDev <alimousa20070@gmail.com>
2025-12-18 18:05:03 +0200

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   api/anime3rb.js

--- api/anime3rb.js
+++ api/anime3rb.js
@@ -1,6 +1,5 @@
 const { getEpisodeStreaming } = require("../dist/main");
-const { getAnime3rb, getEpisodeByNumber, getEpisodeStreams } = require("../dist/anime3rb");
-const { scrapeResponses } = require("../src/tt");
+const { getAnime3rb, getEpisodeByNumber, getEpisodeStreams } = require("../dist/anime3rb")
 module.exports = async (req, res) => {
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
@@ -16,28 +15,27 @@ module.exports = async (req, res) => {
         .json({ error: "AnilistID and ep parameters are required" });
     }
 
-    const data = await scrapeResponses("https://anime3rb.com")
-    // const anime3rb = await getAnime3rb(id)
-    // const animeSlug = anime3rb.slug
-    // console.log(animeSlug)
-    // if(!animeSlug){
-    //     return res.status(500).json({ error: "Failed to find this anime", message: error.message });
-    // }
-    // const animeEpisode = await getEpisodeByNumber(animeSlug,ep)
-    // if(animeEpisode.found !== true || !animeEpisode.episode?.url){
-    //     return res.status(500).json({ error: "Failed to find this ep", message: animeEpisode.message });
-    // }
-    // const episodeUrl = animeEpisode.episode?.url
-    // console.log(episodeUrl)
-    // const episode_streams = await getEpisodeStreams(episodeUrl)
-    // console.log(episode_streams)
-    // if(!episode_streams){
-    //     return res.status(500).json({ error: "Failed to find stream link try again later", message: animeEpisode.message });
-    // }
+    const anime3rb = await getAnime3rb(id)
+    const animeSlug = anime3rb.slug
+    console.log(animeSlug)
+    if(!animeSlug){
+        return res.status(500).json({ error: "Failed to find this anime", message: error.message });
+    }
+    const animeEpisode = await getEpisodeByNumber(animeSlug,ep)
+    if(animeEpisode.found !== true || !animeEpisode.episode?.url){
+        return res.status(500).json({ error: "Failed to find this ep", message: animeEpisode.message });
+    }
+    const episodeUrl = animeEpisode.episode?.url
+    console.log(episodeUrl)
+    const episode_streams = await getEpisodeStreams(episodeUrl)
+    console.log(episode_streams)
+    if(!episode_streams){
+        return res.status(500).json({ error: "Failed to find stream link try again later", message: animeEpisode.message });
+    }
 
-    // return res
-    // .status(200)
-    // .json({ episode_streams });
+    return res
+    .status(200)
+    .json({ episode_streams });
   } catch (error) {
     return res.status(500).json({ error: "Failed", message: error.message });
   }
